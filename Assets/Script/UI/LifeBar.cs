5e03445fbb70ce73dd09d88666ceb89651ca7b7b
DESKTOP-IADNC8M\admin <mori@sie.ics.saitama-u.ac.jp>
2018-11-18 11:59:41 +0900

    HPMPバーの作成

---
+++ Assets/Script/UI/LifeBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour
+{
+    public TestPlayer player;
+
+    public int mode = 0;//0ならHP　1ならMP
+    Slider sl;
+
+    // Use this for initialization
+    void Start()
+    {
+        sl = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mode == 0)
+        {
+           
+            sl.value = ((float)player.status.HP / 100.0f);
+        }
+        if (mode == 1)
+        {
+            sl.value = (float)player.status.MP / 100.0f;
+        }
+    }
+}
