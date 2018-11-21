95657de549eb7d8f1cb5ed7e658e2a0918299891
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-11-22 01:06:56 +0900

    BossAttack2大体

--- Assets/Script/Charactor/Enemy/Boss/Boss.cs
+++ Assets/Script/Charactor/Enemy/Boss/Boss.cs
@@ -5,14 +5,21 @@ public class Boss : TestEnemy
 {
     public float illuminatetime;
 
+    public GameObject NullMagicCircle;
     public GameObject DarkArm;
+    public TestPlayer player;
 
+    public GameObject CameraPrefab;
+    Color tmp_color;
     // Use this for initialization
     void Start()
     {
+        CameraPrefab = GameObject.Find("Main Camera");
+        player = GameObject.Find("TestPlayer").GetComponent<TestPlayer>();
+
         StartEnemy();
         ChangeMode(3);
-
+        
     }
 
     // Update is called once per frame
@@ -37,15 +44,38 @@ public class Boss : TestEnemy
 
     public void Dissable()
     {
+        tmp_color = renderer.color;
         renderer.color = Color.clear;
+        transform.localScale = new Vector3(0, 0, 0);
     }
 
     public void MakeDarkHand()
     {
+
         GameObject g = Instantiate(DarkArm);
-        g.transform.position = transform.position;
-        g.transform.Translate(new Vector3(Random.Range(-10f, 10f), 0, 0));
-        
+        //g.transform.position = player.transform.position;
+        g.transform.position = new Vector3(CameraPrefab.transform.position.x, player.transform.position.y, player.transform.position.z);
+        g.transform.Translate(new Vector3(Random.Range(-7f, 7f), 0, 0));
+
+    }
+    new Transform tr;
+
+    public void MakeNullMagicCircle()
+    {
+        GameObject g = Instantiate(NullMagicCircle);
+        g.transform.position = new Vector3(CameraPrefab.transform.position.x, player.transform.position.y, player.transform.position.z);
+        g.transform.Translate(new Vector3(Random.Range(-7f, 7f), 0, 0));
+
+        tr = g.transform;
+
     }
 
+
+    public void Appear()
+    {
+        transform.position = tr.position;
+        renderer.color = tmp_color;
+        transform.localScale = new Vector3(2.5f, 2.5f, 1);
+        ChangeMode(8);
+    }
 }
---
+++ Assets/Script/Charactor/Enemy/Boss/Boss_Assalt.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss_Assalt : E_ModeBase
+{
+    public int p = 0;
+    public Vector3 attack_offset;
+    new GameObject player;
+    Boss _enemy;
+    public override void Mode_Start(Charactor _obj)
+    {
+        _enemy = _obj.GetComponent<Boss>();
+
+
+        player = GameObject.Find("TestPlayer");
+        base.Mode_Start(_obj);
+
+        MakeHitBox(_obj);
+        //前方に突っ込む
+        Rigidbody2D rb = _obj.GetComponent<Rigidbody2D>();
+
+        if (_enemy.player.transform.position.x > _enemy.transform.position.x)
+        {
+            //→
+            rb.AddForce(new Vector2(800, 0));
+            _obj.transform.localScale = new Vector3(2.5f, _obj.transform.localScale.y, _obj.transform.localScale.z);
+        }
+        else
+        {
+            rb.AddForce(new Vector2(-800, 0));
+            _obj.transform.localScale = new Vector3(-2.5f, _obj.transform.localScale.y, _obj.transform.localScale.z);
+
+        }
+
+    }
+
+    public override void Mode_Update(Charactor _obj)
+    {
+
+
+
+        base.Mode_Update(_obj);
+
+
+
+    }
+
+    public override void MakeHitBox(Charactor _obj)
+    {
+        _obj.hitbox[0] = GameObject.Instantiate(Attack[0], _obj.transform.position, Quaternion.identity) as GameObject;
+        _obj.hitbox[0].transform.parent = _obj.transform;
+
+
+
+    }
+
+}
